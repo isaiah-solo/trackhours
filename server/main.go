35b860f4feca03a21b14b42e9d2d694e44c9ebe4
Isaiah Solomon <isaiah.c.solomon@gmail.com>
2019-03-07 22:50:22 -0800

    fix for backend

--- server/main.go
+++ server/main.go
@@ -41,8 +41,9 @@ var eventNames = [...]string{
 func main() {
   router := gin.Default()
   router.Use(static.Serve("/static", static.LocalFile("../view/build/static", true)))
+  rootDir = "../view/build"
   fileList := []string{}
-  filepath.Walk("../view/build", func(path string, f os.FileInfo, err error) error {
+  filepath.Walk(rootDir, func(path string, f os.FileInfo, err error) error {
     if !(f.IsDir()) {
       fileList = append(fileList, path)
     }
