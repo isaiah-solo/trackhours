8ee87b9ba99ec9bef67cf7c753358708f907fa4f
Isaiah Solomon <isaiah.c.solomon@gmail.com>
2019-09-29 04:10:46 -0700

    fixed render bug for login

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import React, {useCallback, useMemo} from 'react';
+import React, {useCallback} from 'react';
 
 import HomePage from './home/HomePage';
 import LoginPage from './login/LoginPage';
 import Page from './component/Page';
 import Query from './component/Query';
 
-function App(props) {
+function App() {
   const errorRenderer = useCallback(
     (error) => <div>Error: {error.message}</div>,
     [],
   );
   const successRenderer = useCallback(
-    ({isLoggedIn}) => (
-      isLoggedIn ? <HomePage /> : <LoginPage />
-    ),
+    ({is_logged_in: isLoggedIn}) => {
+      return isLoggedIn ? <HomePage /> : <LoginPage />
+    },
     [],
   );
   return (
