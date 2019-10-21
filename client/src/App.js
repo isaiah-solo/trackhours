03f91198850f4939ec7d75d90f6a5d7c8dadc89c
Isaiah Solomon <isaiah.c.solomon@gmail.com>
2019-10-21 02:41:07 -0700

    added react router for login redirecting

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
 // @flow strict
 
+import type {Element} from 'react';
+
 import React, {useCallback} from 'react';
+import {
+  BrowserRouter as Router,
+  Link,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 
 import HomePage from './home/HomePage';
 import LoginPage from './login/LoginPage';
@@ -23,9 +32,33 @@ function App() {
     [],
   );
   const successRenderer = useCallback(
-    ({is_logged_in: isLoggedIn}: Response) => {
-      return isLoggedIn ? <HomePage /> : <LoginPage />
-    },
+    ({is_logged_in: isLoggedIn}: Response): Element<typeof Router> => (
+      <Router>
+        <Route exact
+          path='/login'
+          render={({location}): Element<typeof LoginPage | typeof Redirect> => (
+            isLoggedIn
+              ? <Redirect to={{
+                pathname: '/home',
+                state: { from: location }
+              }} />
+              : <LoginPage />
+          )} />
+        <Route exact
+          path='/home'
+          render={({location}): Element<typeof HomePage | typeof Redirect> => (
+            isLoggedIn
+              ? <HomePage />
+              : <Redirect to={{
+                pathname: '/login',
+                state: { from: location }
+              }} />
+          )} />
+        <Redirect to={{
+          pathname: '/home',
+        }} />
+      </Router>
+    ),
     [],
   );
   return (
